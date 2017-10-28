0b576e298494d7299ce1b7420557819e5f6d2054
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-29 08:10:14 +0900

    Create login form

--- semasky/src/app/view-modules/welcome/welcome.module.ts
+++ semasky/src/app/view-modules/welcome/welcome.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { WelcomeComponent } from './welcome.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { GlobalModule } from '../global/global.module';
 
 @NgModule({
   declarations: [
-    WelcomeComponent
+    WelcomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     WelcomeRoutingModule,
-    GlobalModule
+    GlobalModule,
+    CommonModule
   ],
   exports: [
     WelcomeRoutingModule
