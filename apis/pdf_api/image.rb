8a6180e388344bf390e78881b301d4aa1beae72c
yamaguchi1024 <yukatkh@yahoo.co.jp>
2017-10-28 18:17:04 +0900

    やっとなんか動く

--- apis/pdf_api/image.rb
+++ apis/pdf_api/image.rb
@@ -1,17 +1,15 @@
+# coding: utf-8
+
 require 'net/http'
 require 'net/https'
 require 'json'
 
-URL = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify"
+URL = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=660d75be90c67448e5bd55d3628a0e7a11c36f08&version=2016-05-20"
 uri = URI.parse(URL)
-
-# first gets
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-
-request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-request.body = {api_key: "660d75be90c67448e5bd55d3628a0e7a11c36f08", url: "http://1.bp.blogspot.com/-8_iInJA-VGo/UgSMUzbH8tI/AAAAAAAAXBk/rfI8OGB16n8/s800/sweets_cookie.png", version: "2016-05-20"}.to_json
-
-response = http.request(request)
-html = response.body
-p html
+https = Net::HTTP.new(uri.host,uri.port)
+https.use_ssl = true
+req = Net::HTTP::Post.new(uri, {"accept-encoding": "identity", "accept-language": "ja"})
+req.set_form({"url" => "https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg"}, "multipart/form-data")
+https.set_debug_output($stdout)
+res = https.request(req)
+puts "Response #{res.code} #{res.message}: #{res.body}"
