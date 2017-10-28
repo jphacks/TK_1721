88deea42f8db2e1fabff30a7826b41457ab658d5
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-28 11:12:58 +0900

    Add style files

--- semaski/src/app/view-modules/welcome/welcome.module.ts
+++ semaski/src/app/view-modules/welcome/welcome.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { WelcomeComponent } from './welcome.component';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
+import { GlobalModule } from '../global/global.module';
+
 @NgModule({
   declarations: [
     WelcomeComponent
   ],
   imports: [
-    WelcomeRoutingModule
+    WelcomeRoutingModule,
+    GlobalModule
   ],
   exports: [
     WelcomeRoutingModule
