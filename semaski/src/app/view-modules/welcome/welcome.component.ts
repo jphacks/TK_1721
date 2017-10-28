ab9f3151a46d7553c582bd74a94a7be88b6d99ef
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-28 12:31:07 +0900

    Redirect to search

--- semaski/src/app/view-modules/welcome/welcome.component.ts
+++ semaski/src/app/view-modules/welcome/welcome.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'welcome',
   templateUrl: './welcome.component.html'
 })
-export class WelcomeComponent { }
+export class WelcomeComponent {
+  constructor(
+    private _router: Router
+  ) {
+    _router.navigate(['/search']);
+  }
+}
