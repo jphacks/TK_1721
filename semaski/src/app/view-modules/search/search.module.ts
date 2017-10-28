95450fe8a98b2c76bce57d967f4a648d75d2f826
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-28 13:24:03 +0900

    Create search box

--- semaski/src/app/view-modules/search/search.module.ts
+++ semaski/src/app/view-modules/search/search.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { SearchComponent } from './search.component';
+import { SearchBoxComponent } from './search-box.component';
 import { SearchRoutingModule } from './search-routing.module';
 
 import { GlobalModule } from '../global/global.module';
 
 @NgModule({
   declarations: [
-    SearchComponent
+    SearchComponent,
+    SearchBoxComponent
   ],
   imports: [
     SearchRoutingModule,
