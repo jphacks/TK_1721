5860b2dafa20886ea1878b92bff884c87d590b79
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-28 15:45:28 +0900

    Can drag and drop files to upload

--- semasky/src/app/view-modules/search/upload.component.ts
+++ semasky/src/app/view-modules/search/upload.component.ts
@@ -4,4 +4,27 @@ import { Component } from '@angular/core';
   selector: 'upload',
   templateUrl: './upload.component.html'
 })
-export class UploadComponent { }
+export class UploadComponent {
+  ngAfterViewInit() {
+    let droppable = document.getElementById('droppable');
+    droppable.addEventListener('dragover', e => {
+      e.stopPropagation();
+      e.preventDefault();
+    });
+    droppable.addEventListener('drop', e => {
+      e.stopPropagation();
+      e.preventDefault();
+
+      let files = e.dataTransfer.files;
+      let data = [];
+      for (let i = 0; i < files.length; i++) {
+        let reader: any = new FileReader();
+        reader.onload = e => {
+          // submit
+        }
+        reader.readAsText(files[i]);
+      }
+      console.log(data);
+    });
+  }
+}
