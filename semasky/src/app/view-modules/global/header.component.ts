d8b9c151af1c9e6a689f71cb94ff7a2ac8b4da39
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-29 06:28:12 +0900

    Create logo

--- semasky/src/app/view-modules/global/header.component.ts
+++ semasky/src/app/view-modules/global/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'semasky-header',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+  @Input() type: string;
+
   logout(event) {
     event.preventDefault();
   }
