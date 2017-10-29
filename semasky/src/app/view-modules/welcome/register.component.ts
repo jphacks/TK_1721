00e33fa788e212a8e4f3ae52ba1620360f17b160
xuzijian629 <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-10-29 12:25:44 +0900

    Bug fix

--- semasky/src/app/view-modules/welcome/register.component.ts
+++ semasky/src/app/view-modules/welcome/register.component.ts
@@ -15,6 +15,7 @@ export class RegisterComponent {
 
   submit(event) {
     event.preventDefault();
+    event.stopPropagation();
     let self = this;
     self._register.register({
       email: event.target[0].value,
